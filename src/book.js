00c576253159d54c1bb916ce113397f44e274074
Daniel Lee <dhtlee@rocketmail.com>
2017-09-03 15:00:07 -0400

    DL: Set default value for books that are not in any shelves.

--- src/book.js
+++ src/book.js
@@ -10,7 +10,7 @@ const Book = (props) => {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${thumbnail}')`}}></div>
           <div className="book-shelf-changer">
-            <select value={shelf} onChange={(event => handleShelfUpdate(props.book, event.target.value))}>
+            <select defaultValue='none' value={shelf} onChange={(event => handleShelfUpdate(props.book, event.target.value))}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>

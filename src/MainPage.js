0322857b760d5ec436f231a37dc358a0847200aa
Daniel Lee <dhtlee@rocketmail.com>
2017-09-03 07:51:12 -0400

    DL: Add shelves for MainPage.

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,7 +3,23 @@ import React, { Component } from 'react';
 class MainPage extends Component {
   render() {
     return (
-      <div>This is the main page!
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

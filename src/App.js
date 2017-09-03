81225a8e90a47bcd8aa0d489bbf70e73022265c9
Daniel Lee <dhtlee@rocketmail.com>
2017-09-03 15:02:26 -0400

    DL: Convert MainPage to stateless component.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import { Route } from 'react-router-dom';
 
 import MainPage from 'main-page';
 import SearchPage from 'search-page';
+import * as BooksAPI from 'api/books';
 import 'css/app.css'
 
 class App extends Component {
+  state = {
+    books: []
+  }
 
   updateShelf = (book, shelf) => {
     console.log(book);
     console.log(shelf);
+
+  componentDidMount = () => {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <MainPage handleShelfUpdate={this.updateShelf}/>
+          <MainPage books={this.state.books} handleShelfUpdate={this.updateShelf}/>
         )}/>
         <Route path='/search' render={() => (
           <SearchPage handleShelfUpdate={this.updateShelf}/>
--- src/main-page.js
+++ src/main-page.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import BookList from 'book-list';
-import * as BooksAPI from 'api/books';
 
 const Shelf = {
   CURRENTLY_READING: 'currentlyReading',
@@ -11,64 +10,51 @@ const Shelf = {
   READ: 'read'
 }
 
-class MainPage extends Component {
-  static propTypes = {
-    handleShelfUpdate: PropTypes.func.isRequired
-  }
-  
-  state = {
-    books: []
-  }
-
-  componentDidMount = () => {
-    BooksAPI.getAll().then(books => {
-      this.setState({ books });
-    });
-  }
-
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <BookList 
-                  list={this.state.books.filter(book => book.shelf === Shelf.CURRENTLY_READING)}
-                  handleShelfUpdate={this.props.handleShelfUpdate}
-                />
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <BookList 
-                  list={this.state.books.filter(book => book.shelf === Shelf.WANT_TO_READ)}
-                  handleShelfUpdate={this.props.handleShelfUpdate}
-                />
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <BookList 
-                  list={this.state.books.filter(book => book.shelf === Shelf.READ)}
-                  handleShelfUpdate={this.props.handleShelfUpdate}
-                />
-              </div>
-            </div>
+const MainPage = (props) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <div className="list-books-content">
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <BookList 
+              list={props.books.filter(book => book.shelf === Shelf.CURRENTLY_READING)}
+              handleShelfUpdate={props.handleShelfUpdate}
+            />
           </div>
-          <div className="open-search">
-            <Link to='/search'/>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Want to Read</h2>
+          <div className="bookshelf-books">
+            <BookList 
+              list={props.books.filter(book => book.shelf === Shelf.WANT_TO_READ)}
+              handleShelfUpdate={props.handleShelfUpdate}
+            />
+          </div>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Read</h2>
+          <div className="bookshelf-books">
+            <BookList 
+              list={props.books.filter(book => book.shelf === Shelf.READ)}
+              handleShelfUpdate={props.handleShelfUpdate}
+            />
           </div>
         </div>
       </div>
-    );
-  }
+      <div className="open-search">
+        <Link to='/search'/>
+      </div>
+    </div>
+  </div>
+)
+
+MainPage.propTypes = {
+  books: PropTypes.array.isRequired,
+  handleShelfUpdate: PropTypes.func.isRequired
 }
 
 export default MainPage;

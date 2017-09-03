964bdad00c8d4e2655d2ea013ad76b8cf29c010e
Daniel Lee <dhtlee@rocketmail.com>
2017-09-03 09:23:07 -0400

    DL: Add search functionality and display search results.

---
+++ src/Book.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Book = (props) => {
+  const { imgLink, title, authors } = props;
+  const thumbnail = imgLink && imgLink.thumbnail ? imgLink.thumbnail : null;
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${thumbnail}')`}}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors && authors.join(', ')}</div>
+      </div>
+    </li>
+  )
+}
+
+export default Book;
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import * as BooksAPI from './BooksAPI'
+import Book from './Book';
+
 class SearchPage extends Component {
+  state = {
+    maxResults: 20,
+    results: []
+  }
+
+  searchBooks = (event) => {
+    const query = event.target.value.trim();
+    if (query.length >= 3) {
+      BooksAPI.search(query, this.state.maxResults)
+        .then(results => {
+          this.setState({ results })
+        })
+    }
+  }
+
   render() {
     return (
       <div className="search-books">
         <div className='search-books-bar'>
           <Link className="close-search" to='/'/>
           <div className='search-books-input-wrapper'>
-            <input type='text' placeholder='Search by title or author'/>
+            <input 
+              type='text'
+              placeholder='Search by title or author'
+              value={this.state.query}
+              onChange={this.searchBooks}
+            />
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
+              { 
+                this.state.results.map((book) => 
+                <Book
+                  imgLink={book.imageLinks}
+                  key={book.id}
+                  title={book.title}
+                  authors={book.authors}  
+                />
+              )}
           </ol>
         </div>
       </div>

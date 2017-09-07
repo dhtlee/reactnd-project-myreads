2a93b7e0bce8ba852ea121c61e24968a6435d1d8
dhtlee <dhtlee@rocketmail.com>
2017-09-06 21:22:25 -0400

    DL: Refactor to reduce code duplication.

--- src/main-page.js
+++ src/main-page.js
@@ -1,48 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import BookShelf from 'bookshelf';
 
-const Shelf = {
-  CURRENTLY_READING: 'currentlyReading',
-  WANT_TO_READ: 'wantToRead',
-  READ: 'read'
-}
+const shelf = [
+  {
+    id: 'currentlyReading',
+    name: 'Currently Reading'
+  },
+  {
+    id: 'wantToRead',
+    name: 'Want to Read'
+  },
+  {
+    id: 'read',
+    name: 'Read'
+  }
+]
 
-const MainPage = (props) => (
-  <div className="list-books">
-    <div className="list-books-title">
-      <h1>MyReads</h1>
-    </div>
-    <div className="list-books-content">
-      <div>
-        <BookShelf 
-          name='Currently Reading' 
-          books={props.books.filter(book => book.shelf === Shelf.CURRENTLY_READING)}
-          handleShelfUpdate={props.handleShelfUpdate}
-        />
-        <BookShelf 
-          name='Want to Read' 
-          books={props.books.filter(book => book.shelf === Shelf.WANT_TO_READ)}
-          handleShelfUpdate={props.handleShelfUpdate}
-        />
-        <BookShelf 
-          name='Read' 
-          books={props.books.filter(book => book.shelf === Shelf.READ)}
-          handleShelfUpdate={props.handleShelfUpdate}
-        />
-      </div>
-      <div className="open-search">
-        <Link to='/search'/>
-      </div>
-    </div>
-  </div>
-)
+class MainPage extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    handleShelfUpdate: PropTypes.func.isRequired
+  }
 
-MainPage.propTypes = {
-  books: PropTypes.array.isRequired,
-  handleShelfUpdate: PropTypes.func.isRequired
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            { shelf.map(shelf => (
+              <BookShelf
+                key={shelf.id}
+                name={shelf.name} 
+                books={this.props.books.filter(book => book.shelf === shelf.id)}
+                handleShelfUpdate={this.props.handleShelfUpdate}/>
+            ))}
+          </div>
+          <div className="open-search">
+            <Link to='/search'/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default MainPage;
